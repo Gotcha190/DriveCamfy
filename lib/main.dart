d324c7969d1459c563702f60f16621436438b6b7
Gotcha190 <konradformella190@gmail.com>
2024-02-14 00:55:38 +0100

    Init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:drive_camfy/views/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        title: 'DriveCamfy',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/': (ctx) => const HomePage(),
+        },
+      );
+    });
+  }
+}
